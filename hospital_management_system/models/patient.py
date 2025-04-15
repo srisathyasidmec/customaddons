139e480e8c02475fe175b9a9b6782bf5b5da735b
srisathyasidmec <srisathya@sidmectech.in>
2025-04-15 18:16:54 +0530

    added

--- hospital_management_system/models/patient.py
+++ hospital_management_system/models/patient.py
@@ -1,30 +1,33 @@
 from odoo import models, fields, api
+from datetime import date
+
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _rec_name = "patient_name"
-    _inherit = ['mail.thread', 'mail.activity.mixin'] #inherits in this DB
+    _inherit = ['mail.thread', 'mail.activity.mixin']  # inherits in this DB
 
-    patient_name=fields.Char(string="Name")
-    age=fields.Integer(string="Age")
-    address=fields.Char(String="Address")
+    patient_name = fields.Char(string="Patient Name")
+    age = fields.Integer(string="Age")
+    address = fields.Char(String="Address")
 
     patient_email = fields.Char(string="Patient Email")
 
-    patient_doctor = fields.Many2one(comodel_name="hospital.doctor", domain=[("email", "!=", False)], string="Doctor Name", tracking=True, required=True, )
-                                    #comodel_name is used as target model this field connects to
-                                    #domain helps in filtering & shows doctors whose mail is present
+    patient_doctor = fields.Many2one(comodel_name="hospital.doctor", domain=[("email", "!=", False)],
+                                     string="Doctor Name", tracking=True, required=True, )
+    # comodel_name is used as target model this field connects to
+    # domain helps in filtering & shows doctors whose mail is present
 
-    doctor_mail = fields.Char(related="patient_doctor.email",string="Doctor email") #compute="compute_doctor_email"
+    doctor_mail = fields.Char(related="patient_doctor.email", string="Doctor email")  # compute="compute_doctor_email"
 
     patient_id = fields.Many2one(comodel_name="res.partner", string="PId from Partner", tracking=True, required=True, )
 
     gender = fields.Selection([("male", "Male"), ("female", "Female")], "Gender")
-                                    #left value stores in Db and right value shows in Odoo UI
+    # left value stores in Db and right value shows in Odoo UI
 
-    admit_date = fields.Date("Admit date") #adds date total calendar
+    admit_date = fields.Date("Admit date")  # adds date total calendar
     is_patient_is_discharged = fields.Boolean("is patient is discharged")
-                                            #boolean is like checkbox. If ticks acts as True else False
+    # boolean is like checkbox. If ticks acts as True else False
     discharge_date = fields.Date("discharge Date")
 
     # #parameter is not needed in doctor_mail field
@@ -38,9 +41,8 @@ class HospitalPatient(models.Model):
     #     for rec in self:
     #         rec.doctor_mail = rec.patient_doctor.email
 
-
     patient_lines = fields.One2many("hospital.patient.line", "patient", "Order lines")
-                        #if we add attribute One2Many then Many2One should also be done
+    # if we add attribute One2Many then Many2One should also be done
 
     user_id = fields.Many2one("res.users", "user", compute="compute_user_company")
     company_id = fields.Many2one("res.company", "Company", compute="compute_user_company")
@@ -50,25 +52,8 @@ class HospitalPatient(models.Model):
             rec.user_id = self.env.user
             rec.company_id = self.env.user.company_id.id
 
-    class HospitalPatientLines(models.Model):
-        _name = "hospital.patient.line"
-
-        product_id = fields.Many2one("product.product", "product Name")
-        qty = fields.Integer("qty")
-        unit_price = fields.Float("Unit price")
-        total=fields.Integer("Total Amount")
-        patient = fields.Many2one("hospital.patient", "patient")
-
-        @api.onchange("unit_price")
-        def total_val(self):
-            for rec in self:
-                rec.total = rec.qty * rec.unit_price
-
-
-
-
-    #to add image from module
-    # image=fields.Image(string="image")
+    # to add image from module
+    image_1920=fields.Binary(string="image")
 
     def send_email(self):
         # print("Hello")
@@ -76,10 +61,25 @@ class HospitalPatient(models.Model):
             template = self.env.ref("hospital_management_system.mail_template_patient_confirm")
             template.send_mail(rec.id, force_send=True)
 
-    #this is field used for status bar purpose
-    status = fields.Selection([("op","OP"),("admit", "Admitted"), ("discharge", "Discharged")], "status", default='op')
+    # this is field used for status bar purpose
+    status = fields.Selection([("op", "OP"), ("admit", "Admitted"), ("discharge", "Discharged")], "status",
+                              default='op', compute="status_update")
 
-    #thsi function used as one smart button action
+    op_date = fields.Date("OP date")
+
+    # @api.onchange("status")
+    def status_update(self):
+        for res in self:
+            today = date.today()
+            if today == res.op_date:
+                res.status = "op"
+            elif today < res.discharge_date:
+                res.status = "admit"
+            else:
+                res.status = "discharge"
+            # print(today)
+
+    # thsi function used as one smart button action
     def view_patient_lines(self):
         self.ensure_one()
         for rec in self:
@@ -90,3 +90,18 @@ class HospitalPatient(models.Model):
                 'domain': [('patient', '=', rec.patient_name)],
                 'type': 'ir.actions.act_window',
             }
+
+
+class HospitalPatientLines(models.Model):
+    _name = "hospital.patient.line"
+
+    product_id = fields.Many2one("product.product", "product Name")
+    qty = fields.Integer("qty")
+    unit_price = fields.Float("Unit price")
+    total = fields.Integer("Total Amount")
+    patient = fields.Many2one("hospital.patient", "patient")
+
+    @api.onchange("unit_price")
+    def total_val(self):
+        for rec in self:
+            rec.total = rec.qty * rec.unit_price
