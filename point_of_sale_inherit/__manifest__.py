850103197b3ab1c6aad8c7b21c0d9d5c05c2f24a
srisathyasidmec <srisathya@sidmectech.in>
2025-04-29 10:15:51 +0530

    task added

---
+++ point_of_sale_inherit/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "POS Modification",
+    "author": "Sri Sathya",
+    "version": "18.0",
+    "depends": ["point_of_sale",],
+    # 'license': 'LGPL-3',
+    'assets': {
+        'point_of_sale._assets_pos': [
+            "point_of_sale_inherit/static/src/js/product_card.js",
+            "point_of_sale_inherit/static/src/xml/product_card.xml",
+            "point_of_sale_inherit/static/src/xml/product_screen.xml",
+        ],
+    },
+}
---
+++ point_of_sale_inherit/static/src/js/product_card.js
@@ -0,0 +1,10 @@
+import { ProductCard } from "@point_of_sale/app/generic_components/product_card/product_card"
+import { patch } from "@web/core/utils/patch";
+
+patch(ProductCard, {
+    props: {
+        ...ProductCard.props,
+        barcode: String,
+        lst_price : { type: Number, optional : true}
+    },
+})
