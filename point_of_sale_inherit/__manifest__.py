483e9575051ddb98190642d28146e66fe71ff0d7
srisathyasidmec <srisathya@sidmectech.in>
2025-04-30 19:56:13 +0530

    task added

--- point_of_sale_inherit/__manifest__.py
+++ point_of_sale_inherit/__manifest__.py
@@ -7,8 +7,13 @@
     'assets': {
         'point_of_sale._assets_pos': [
             "point_of_sale_inherit/static/src/js/product_card.js",
+            "point_of_sale_inherit/static/src/js/receipt_screen_changes.js",
+            "point_of_sale_inherit/static/src/xml/receipt_screen_changes.xml",
             "point_of_sale_inherit/static/src/xml/product_card.xml",
             "point_of_sale_inherit/static/src/xml/product_screen.xml",
         ],
     },
+    # "data" : [
+    #     "report/report_recipt_screen.xml",
+    # ],
 }
--- point_of_sale_inherit/static/src/js/product_card.js
+++ point_of_sale_inherit/static/src/js/product_card.js
@@ -1,10 +1,21 @@
 import { ProductCard } from "@point_of_sale/app/generic_components/product_card/product_card"
+//import { ProductChangeQuantity } from "@stock/wizard/stock_change_product_qty"
 import { patch } from "@web/core/utils/patch";
 
 patch(ProductCard, {
     props: {
         ...ProductCard.props,
-        barcode: String,
-        lst_price : { type: Number, optional : true}
+        barcode : String ,
+        lst_price : { type: Number, optional : true},
     },
 })
+
+//export class MyCustomWizard extends ProductChangeQuantity {
+//    super.ProductChangeQuantity()
+//}
+//patch(MyCustomWizard, {
+//    props: {
+//        ...ProductCard.props,
+//        new_quantity : { type: Number, optional : true},
+//    },
+//})
